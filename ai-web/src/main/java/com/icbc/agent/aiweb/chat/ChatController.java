bcca11e8d6baa743404d2906874063b306d5dcaa
GeorgeChan <327052186@qq.com>
2026-06-15 21:04:56 +0800

    添加聊天接口+响应类

--- ai-web/src/main/java/com/icbc/agent/aiweb/chat/ChatController.java
+++ ai-web/src/main/java/com/icbc/agent/aiweb/chat/ChatController.java
@@ -1,16 +1,23 @@
 package com.icbc.agent.aiweb.chat;
 
 import com.icbc.agent.aicommon.chat.Chat;
+import com.icbc.agent.aiweb.common.Result;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @ComponentScan("com.icbc.agent.*")
 public class ChatController {
 
-    @GetMapping("/chat")
-    public String chat() {
-        return new Chat().getCommon();
+    @PostMapping("ai/message")
+    public Result<String> chat() {
+        return Result.success(new Chat().getCommon());
+    }
+
+
+    @PostMapping("/auth/login")
+    public String login() {
+        return "1";
     }
 }
